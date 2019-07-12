4dd69a212ca9bf5ace96c2bf8502b7f8143df18e
Bhumi Patel <bhumip214@gmail.com>
2019-07-11 18:00:04 -0700

    updated DisplayList component

--- src/Components/DisplayList.js
+++ src/Components/DisplayList.js
@@ -1,43 +1,60 @@
 import React from "react";
 import pullRequestsData from "./PRLists";
+import { groupByMonth } from "./helper";
 
 class DisplayList extends React.Component {
   constructor() {
     super();
     this.state = {
       pullRequests: pullRequestsData.data.viewer.pullRequests.nodes,
-      username: pullRequestsData.data.viewer.login
+      username: pullRequestsData.data.viewer.login,
+      months: groupByMonth(pullRequestsData.data.viewer.pullRequests.nodes)
     };
   }
   render() {
     return (
       <div className="listsWrapper">
-        {this.state.pullRequests.map((pullRequest, index) => {
+        {this.state.months.map((month, index) => {
+          console.log(month);
+          if (month.pullRequests.length === 0) {
+            return null;
+          }
           return (
-            <li className="lists" key={index}>
-              <a
-                className="ownerRepoName"
-                href={`https://github.com/${
-                  pullRequest.repository.owner.login
-                }/${pullRequest.repository.name}`}
-              >
-                {pullRequest.repository.owner.login}/
-                {pullRequest.repository.name}
-              </a>
-              <a
-                className="PRName"
-                href={`https://github.com/${
-                  pullRequest.repository.owner.login
-                }/${pullRequest.repository.name}/pull/${pullRequest.number}`}
-              >
-                {pullRequest.title}
-              </a>
-              <div className="subtitle">
-                #{pullRequest.number}{" "}
-                {new Date(pullRequest.createdAt).toDateString()} by{" "}
-                <a href="">{this.state.username}</a>
-              </div>
-            </li>
+            <div>
+              <h1 key={index} className="lists">
+                {month.month}
+              </h1>
+              {month.pullRequests.map((pullRequest, index) => {
+                return (
+                  <li className="lists" key={index}>
+                    <a
+                      className="ownerRepoName"
+                      href={`https://github.com/${
+                        pullRequest.repository.owner.login
+                      }/${pullRequest.repository.name}`}
+                    >
+                      {pullRequest.repository.owner.login}/
+                      {pullRequest.repository.name}
+                    </a>
+                    <a
+                      className="PRName"
+                      href={`https://github.com/${
+                        pullRequest.repository.owner.login
+                      }/${pullRequest.repository.name}/pull/${
+                        pullRequest.number
+                      }`}
+                    >
+                      {pullRequest.title}
+                    </a>
+                    <div className="subtitle">
+                      #{pullRequest.number}{" "}
+                      {new Date(pullRequest.createdAt).toDateString()} by{" "}
+                      <a href="">{this.state.username}</a>
+                    </div>
+                  </li>
+                );
+              })}
+            </div>
           );
         })}
       </div>
