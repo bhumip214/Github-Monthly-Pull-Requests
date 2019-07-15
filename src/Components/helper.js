39c2a89b2800e49e18af0fac5371e75b81b13e26
Bhumi Patel <bhumip214@gmail.com>
2019-07-14 21:48:23 -0700

    use lodash to groupBy

--- src/Components/helper.js
+++ src/Components/helper.js
@@ -1,75 +1,82 @@
-export function groupByMonth(lists) {
-  const months = [];
-  const jan = [];
-  const feb = [];
-  const march = [];
-  const april = [];
-  const may = [];
-  const june = [];
-  const july = [];
-  const aug = [];
-  const sept = [];
-  const oct = [];
-  const nov = [];
-  const dec = [];
+import _ from "lodash";
 
-  for (let i = 0; i < lists.length; i++) {
-    const month = new Date(lists[i].createdAt).getMonth();
-    if (month === 0) {
-      jan.push(lists[i]);
-    } else if (month === 1) {
-      feb.push(lists[i]);
-    } else if (month === 2) {
-      march.push(lists[i]);
-    } else if (month === 3) {
-      april.push(lists[i]);
-    } else if (month === 4) {
-      may.push(lists[i]);
-    } else if (month === 5) {
-      june.push(lists[i]);
-    } else if (month === 6) {
-      july.push(lists[i]);
-    } else if (month === 7) {
-      aug.push(lists[i]);
-    } else if (month === 8) {
-      sept.push(lists[i]);
-    } else if (month === 9) {
-      oct.push(lists[i]);
-    } else if (month === 10) {
-      nov.push(lists[i]);
-    } else {
-      dec.push(lists[i]);
-    }
+function formatMonth(date) {
+  const year = date.substr(0, 4);
+  let month = "";
+
+  switch (date.substr(5, 7)) {
+    case "01":
+      month = "January";
+      break;
+    case "02":
+      month = "February";
+      break;
+    case "03":
+      month = "March";
+      break;
+    case "04":
+      month = "April";
+      break;
+    case "05":
+      month = "May";
+      break;
+    case "06":
+      month = "June";
+      break;
+    case "07":
+      month = "July";
+      break;
+    case "08":
+      month = "August";
+      break;
+    case "09":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+    case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
   }
 
-  months.push({ month: "January", pullRequests: jan });
-  months.push({ month: "February", pullRequests: feb });
-  months.push({ month: "March", pullRequests: march });
-  months.push({ month: "April", pullRequests: april });
-  months.push({ month: "May", pullRequests: may });
-  months.push({ month: "June", pullRequests: june });
-  months.push({ month: "July", pullRequests: july });
-  months.push({ month: "August", pullRequests: aug });
-  months.push({ month: "September", pullRequests: sept });
-  months.push({ month: "October", pullRequests: oct });
-  months.push({ month: "November", pullRequests: nov });
-  months.push({ month: "December", pullRequests: dec });
+  return `${month} ${year}`;
+}
 
-  months.sort(function(a, b) {
-    if (a.pullRequests.length === 0 || b.pullRequests.length === 0) {
-      return 0;
-    } else {
-      const aDate = a.pullRequests[0].createdAt;
-      const bDate = b.pullRequests[0].createdAt;
-      if (aDate > bDate) {
-        return 1;
-      } else if (aDate < bDate) {
-        return -1;
-      } else {
-        return 0;
-      }
-    }
+/**
+
+PR = {
+  id: string
+  title: string
+  number: number
+  repository: object
+  createdAt: string
+}
+
+Input:
+lists = PR[]
+
+Output:
+[
+  {
+    month: string
+    pullRequests: PR[]
+  }
+]
+ */
+
+export function groupByMonth(lists) {
+  const grouped = _.groupBy(lists, item => {
+    return item.createdAt.substr(0, 7);
   });
-  //   console.log(months);
-  return months.reverse();
+
+  return Object.entries(grouped).map(entry => {
+    return {
+      month: formatMonth(entry[0]),
+      pullRequests: entry[1]
+    };
+  });
 }
